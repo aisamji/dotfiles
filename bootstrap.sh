f602f5645d82105caf40ccaa511ce3554b37a679
Ali Samji <aisamji@users.noreply.github.com>
2026-04-16 19:51:43 -0500

    Add simple bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+set -euo pipefail
+
+if ! command -v ansible-playbook > /dev/null; then
+  echo 'Installing Ansible'
+  if [[ "$(uname)" == "Darwin" ]]; then
+    # Mac
+    if ! command -v brew > /dev/null; then
+      echo 'Installing Homebrew'
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+      # Add to PATH for Apple Silicon
+      if [[ $(uname -m) == "arm64" ]]; then
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+      fi
+    fi
+
+    brew install ansible
+  else
+    # Linux (assume Arch, no other distros in-use yet)
+    sudo pacman -Syu ansible
+  fi
+else
+  echo 'Ansible already installed'
+fi
+
+# TODO: Install Ansible and Python requirements
+
+# TODO: Clone the dotfiles repo, to support downloading and running jsut the script.
+case "$1" in
+  personal)
+    ansible-playbook playbooks/personal.yml
+  ;;
+  work)
+    ansible-playbook playbooks/work.yml
+  ;;
+  *)
+    echo 'Unrecognized environment' >&2
+    exit 1
+  ;;
+esac
+
