ba50b2d63ece71afe5def2b5993f9d105bf0b13e
Ali Samji <aisamji@users.noreply.github.com>
2026-04-23 12:18:29 -0500

    Add key map for LSP logs.

--- playbooks/roles/nvim/files/nvim/lua/mappings.lua
+++ playbooks/roles/nvim/files/nvim/lua/mappings.lua
@@ -11,3 +11,6 @@ map("n", "<leader>gB", function()
   vim.cmd.Gitsigns "blame"
 end, { desc = "Git blame file." })
 map("n", "=", vim.lsp.buf.format, { desc = "Format file" })
+map("n", "<leader>ll", function()
+  vim.cmd("tabnew " .. vim.lsp.log.get_filename())
+end, { desc = "Check LSP logs" })
