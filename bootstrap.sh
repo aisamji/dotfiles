2870eacedaabcc9d040bc0d801507a9e497e0b1d
Ali Samji <aisamji@users.noreply.github.com>
2026-05-06 08:14:47 -0500

    Formatting fixes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,29 @@
 #! /bin/sh
 set -euo pipefail
 
-if ! command -v ansible-playbook > /dev/null; then
-  echo 'Installing Ansible'
-  if [[ "$(uname)" == "Darwin" ]]; then
-    # Mac
-    if ! command -v brew > /dev/null; then
-      echo 'Installing Homebrew'
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-      # Add to PATH for Apple Silicon
-      if [[ $(uname -m) == "arm64" ]]; then
-          eval "$(/opt/homebrew/bin/brew shellenv)"
-      fi
-    fi
+if ! command -v ansible-playbook >/dev/null; then
+    echo 'Installing Ansible'
+    if [[ "$(uname)" == "Darwin" ]]; then
+        # Mac
+        if ! command -v brew >/dev/null; then
+            echo 'Installing Homebrew'
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            # Add to PATH for Apple Silicon
+            if [[ $(uname -m) == "arm64" ]]; then
+                eval "$(/opt/homebrew/bin/brew shellenv)"
+            fi
+        fi
 
-    brew install ansible
-  else
-    # Linux (assume Arch, no other distros in-use yet)
-    sudo pacman -Syu ansible
-  fi
+        brew install ansible
+    else
+        # Linux (assume Arch, no other distros in-use yet)
+        sudo pacman -Syu ansible
+    fi
 else
-  echo 'Ansible already installed'
+    echo 'Ansible already installed'
 fi
 
 # TODO: Install Ansible and Python requirements
 
 # TODO: Clone the dotfiles repo, to support downloading and running just the script.
 ansible-playbook --diff playbooks/site.yml --inventory inventories/host.yml --ask-become-pass --limit "$1"
-
