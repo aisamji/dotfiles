f577e5dd59ba220a2a910837f007d52a01c92ed5
Ali Samji <aisamji@users.noreply.github.com>
2026-05-07 09:28:12 -0500

    Add keymap for Lsp log.

--- playbooks/roles/nvim/files/nvim/lua/config/mappings.lua
+++ playbooks/roles/nvim/files/nvim/lua/config/mappings.lua
@@ -1,4 +1,9 @@
 vim.keymap.set("n", "grd", vim.lsp.buf.definition, { desc = "vim.lsp.buf.definition()" })
+vim.keymap.set("n", "grl", function()
+    vim.cmd.tabnew(vim.lsp.log.get_filename())
+    vim.bo.readonly = true
+    vim.bo.modifiable = false
+end, { desc = "vim.lsp.log" })
 
 vim.keymap.set("n", "<leader>ds", function() vim.diagnostic.setloclist() end, { desc = "Show file diagnostics" })
 vim.keymap.set("n", "<leader>dS", function() vim.diagnostic.setqflist() end, { desc = "Show workspace diagnostics" })
