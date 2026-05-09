d66ed360d6b6a918011d9387886d8a038f5f04f9
Ali Samji <aisamji@users.noreply.github.com>
2026-05-09 09:30:28 -0500

    Add lazy-dev.nvim for lua_ls workspace hacking.

--- playbooks/roles/nvim/files/nvim/lua/plugins/editing.lua
+++ playbooks/roles/nvim/files/nvim/lua/plugins/editing.lua
@@ -52,4 +52,15 @@ return {
             "FelipeLema/cmp-async-path",
         },
     },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
 }
