a55a05d4879d5e8b76e80c91014d6a0e604c2ac0
Ali Samji <aisamji@users.noreply.github.com>
2026-05-14 10:04:39 -0500

    doc(neovim): Add type annotations for conform

--- playbooks/files/nvim/lua/plugins/editing.lua
+++ playbooks/files/nvim/lua/plugins/editing.lua
@@ -2,6 +2,8 @@ return {
     {
         "stevearc/conform.nvim",
         event = "BufWritePre",
+        ---@module 'conform'
+        ---@type conform.setupOpts
         opts = {
             formatters_by_ft = {
                 lua = { "stylua" },
