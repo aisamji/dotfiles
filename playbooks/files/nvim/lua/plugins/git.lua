e67c125015b8a2eca090d0691ec9685ac9effcca
Ali Samji <aisamji@users.noreply.github.com>
2026-05-14 14:47:51 -0500

    feat(neovim): Add octo.nvim

--- playbooks/files/nvim/lua/plugins/git.lua
+++ playbooks/files/nvim/lua/plugins/git.lua
@@ -3,8 +3,20 @@ return {
         "lewis6991/gitsigns.nvim",
         event = "VeryLazy",
         keys = {
-            { "<leader>gb", function() vim.cmd.Gitsigns "blame_line" end, desc = "Git blame line" },
-            { "<leader>gB", function() vim.cmd.Gitsigns "blame" end,      desc = "Git blame file" },
+            {
+                "<leader>gb",
+                function()
+                    vim.cmd.Gitsigns "blame_line"
+                end,
+                desc = "Git blame line",
+            },
+            {
+                "<leader>gB",
+                function()
+                    vim.cmd.Gitsigns "blame"
+                end,
+                desc = "Git blame file",
+            },
         },
         config = true,
     },
@@ -52,7 +64,7 @@ return {
             },
         },
         dependencies = {
-            "nvim-lua/plenary.nvim",   -- required
+            "nvim-lua/plenary.nvim", -- required
             "sindrets/diffview.nvim",
             "nvim-telescope/telescope.nvim", -- optional
         },
@@ -76,12 +88,17 @@ return {
                 keymaps = {
                     disable_defaults = true, -- Disable the default keymaps
                     view = {
-                        { "n", "]f",        actions.select_next_entry,  { desc = "Open the diff for the next file" } },
-                        { "n", "[f",        actions.select_prev_entry,  { desc = "Open the diff for the previous file" } },
-                        { "n", "[F",        actions.select_first_entry, { desc = "Open the diff for the first file" } },
-                        { "n", "]F",        actions.select_last_entry,  { desc = "Open the diff for the last file" } },
-                        { "n", "<leader>e", actions.focus_files,        { desc = "Bring focus to the file panel" } },
-                        { "n", "<C-n",      actions.toggle_files,       { desc = "Toggle the file panel." } },
+                        { "n", "]f", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+                        {
+                            "n",
+                            "[f",
+                            actions.select_prev_entry,
+                            { desc = "Open the diff for the previous file" },
+                        },
+                        { "n", "[F", actions.select_first_entry, { desc = "Open the diff for the first file" } },
+                        { "n", "]F", actions.select_last_entry, { desc = "Open the diff for the last file" } },
+                        { "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
+                        { "n", "<C-n", actions.toggle_files, { desc = "Toggle the file panel." } },
                         {
                             "n",
                             "[x",
@@ -163,24 +180,64 @@ return {
                         { "n", "g?", actions.help { "view", "diff3" }, { desc = "Open the help panel" } },
                     },
                     file_panel = {
-                        { "n", "j",         actions.next_entry,         { desc = "Bring the cursor to the next file entry" } },
-                        { "n", "<down>",    actions.next_entry,         { desc = "Bring the cursor to the next file entry" } },
-                        { "n", "k",         actions.prev_entry,         { desc = "Bring the cursor to the previous file entry" } },
-                        { "n", "<up>",      actions.prev_entry,         { desc = "Bring the cursor to the previous file entry" } },
-                        { "n", "<cr>",      actions.select_entry,       { desc = "Open the diff for the selected entry" } },
-                        { "n", "s",         actions.toggle_stage_entry, { desc = "Stage / unstage the selected entry" } },
-                        { "n", "S",         actions.stage_all,          { desc = "Stage all entries" } },
-                        { "n", "U",         actions.unstage_all,        { desc = "Unstage all entries" } },
-                        { "n", "X",         actions.restore_entry,      { desc = "Restore entry to the state on the left side" } },
-                        { "n", "]f",        actions.select_next_entry,  { desc = "Open the diff for the next file" } },
-                        { "n", "[f",        actions.select_prev_entry,  { desc = "Open the diff for the previous file" } },
-                        { "n", "[F",        actions.select_first_entry, { desc = "Open the diff for the first file" } },
-                        { "n", "]F",        actions.select_last_entry,  { desc = "Open the diff for the last file" } },
-                        { "n", "<leader>e", actions.focus_files,        { desc = "Bring focus to the file panel" } },
-                        { "n", "<C-n",      actions.toggle_files,       { desc = "Toggle the file panel" } },
-                        { "n", "g?",        actions.help "file_panel",  { desc = "Open the help panel" } },
-                        { "n", "[x",        actions.prev_conflict,      { desc = "Go to the previous conflict" } },
-                        { "n", "]x",        actions.next_conflict,      { desc = "Go to the next conflict" } },
+                        {
+                            "n",
+                            "j",
+                            actions.next_entry,
+                            { desc = "Bring the cursor to the next file entry" },
+                        },
+                        {
+                            "n",
+                            "<down>",
+                            actions.next_entry,
+                            { desc = "Bring the cursor to the next file entry" },
+                        },
+                        {
+                            "n",
+                            "k",
+                            actions.prev_entry,
+                            { desc = "Bring the cursor to the previous file entry" },
+                        },
+                        {
+                            "n",
+                            "<up>",
+                            actions.prev_entry,
+                            { desc = "Bring the cursor to the previous file entry" },
+                        },
+                        {
+                            "n",
+                            "<cr>",
+                            actions.select_entry,
+                            { desc = "Open the diff for the selected entry" },
+                        },
+                        {
+                            "n",
+                            "s",
+                            actions.toggle_stage_entry,
+                            { desc = "Stage / unstage the selected entry" },
+                        },
+                        { "n", "S", actions.stage_all, { desc = "Stage all entries" } },
+                        { "n", "U", actions.unstage_all, { desc = "Unstage all entries" } },
+                        {
+                            "n",
+                            "X",
+                            actions.restore_entry,
+                            { desc = "Restore entry to the state on the left side" },
+                        },
+                        { "n", "]f", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+                        {
+                            "n",
+                            "[f",
+                            actions.select_prev_entry,
+                            { desc = "Open the diff for the previous file" },
+                        },
+                        { "n", "[F", actions.select_first_entry, { desc = "Open the diff for the first file" } },
+                        { "n", "]F", actions.select_last_entry, { desc = "Open the diff for the last file" } },
+                        { "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
+                        { "n", "<C-n", actions.toggle_files, { desc = "Toggle the file panel" } },
+                        { "n", "g?", actions.help "file_panel", { desc = "Open the help panel" } },
+                        { "n", "[x", actions.prev_conflict, { desc = "Go to the previous conflict" } },
+                        { "n", "]x", actions.next_conflict, { desc = "Go to the next conflict" } },
                         {
                             "n",
                             "<localleader>O",
@@ -213,8 +270,8 @@ return {
                         },
                     },
                     option_panel = {
-                        { "n", "<esc>", actions.close,               { desc = "Close the panel" } },
-                        { "n", "g?",    actions.help "option_panel", { desc = "Open the help panel" } },
+                        { "n", "<esc>", actions.close, { desc = "Close the panel" } },
+                        { "n", "g?", actions.help "option_panel", { desc = "Open the help panel" } },
                     },
                     help_panel = {
                         { "n", "<esc>", actions.close, { desc = "Close help menu" } },
@@ -222,5 +279,48 @@ return {
                 },
             }
         end,
+        {
+            "pwntester/octo.nvim",
+            cmd = "Octo",
+            opts = {
+                picker = "telescope",
+                -- bare Octo command opens picker of commands
+                enable_builtin = true,
+            },
+            keys = {
+                {
+                    "<leader>oi",
+                    "<CMD>Octo issue list<CR>",
+                    desc = "List GitHub Issues",
+                },
+                {
+                    "<leader>op",
+                    "<CMD>Octo pr list<CR>",
+                    desc = "List GitHub PullRequests",
+                },
+                {
+                    "<leader>od",
+                    "<CMD>Octo discussion list<CR>",
+                    desc = "List GitHub Discussions",
+                },
+                {
+                    "<leader>on",
+                    "<CMD>Octo notification list<CR>",
+                    desc = "List GitHub Notifications",
+                },
+                {
+                    "<leader>os",
+                    function()
+                        require("octo.utils").create_base_search_command { include_current_repo = true }
+                    end,
+                    desc = "Search GitHub",
+                },
+            },
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-telescope/telescope.nvim",
+                "nvim-tree/nvim-web-devicons",
+            },
+        },
     },
 }
