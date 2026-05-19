24ec18b5c6efe19301d2a6887631a0258745bc9f
Ali Samji <aisamji@users.noreply.github.com>
2026-05-19 08:30:24 -0500

    feat(neovim): Add autocmd to sync back lazy-lock

--- playbooks/files/nvim/lua/config/autocmds.lua
+++ playbooks/files/nvim/lua/config/autocmds.lua
@@ -30,3 +30,19 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.keymap.set("n", "<CR>", "<CR>:cclose<CR>:lclose<CR>", { buffer = true, silent = true })
     end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = { "LazyUpdate", "LazyInstall", "LazyClean" },
+    desc = "Commit lazy-lock to dotfiles repo.",
+    callback = function(_)
+        local src_file = "~/.config/nvim/lazy-lock.json"
+        local dest_file = "~/.dotfiles/playbooks/files/nvim/lazy-lock.json"
+        local dotfiles_repo = "~/.dotfiles"
+
+        if not vim.uv.fs_stat(dest_file) or (vim.fn.system { "diff", src_file, dest_file } ~= "") then
+            vim.uv.fs_copyfile(src_file, dest_file, { excl = false, ficlone = false, ficlone_force = false })
+            vim.fn.system { "git", "add", "-C", dotfiles_repo, dest_file }
+            vim.fn.system { "git", "commit", "-C", dotfiles_repo, "-m", "chore(neovim): Lazy update" }
+        end
+    end,
+})
