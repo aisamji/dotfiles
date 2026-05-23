d97bea282195afe7241b4b879d4b4453e7f9fbe8
Ali Samji <aisamji@users.noreply.github.com>
2026-05-23 00:24:37 -0500

    fix(neovim): treesitter syntax highlighting.

--- playbooks/files/nvim/lua/plugins/treesitter.lua
+++ playbooks/files/nvim/lua/plugins/treesitter.lua
@@ -8,18 +8,33 @@ return {
             require("nvim-treesitter.parsers").starlark = {
                 install_info = {
                     url = "https://github.com/tree-sitter-grammars/tree-sitter-starlark",
+                    queries = "queries",
+                    generate_from_json = false,
                     files = { "src/parser.c", "src/scanner.c" },
                     branch = "master",
                 },
             }
 
-            vim.api.nvim_create_autocmd('FileType', {
-                pattern = '*',
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "*",
                 callback = function(event)
                     local lang = vim.treesitter.language.get_lang(event.match)
-                    if lang then
-                        require('nvim-treesitter').install(lang)
+                    local parsers = require "nvim-treesitter.parsers"
+                    local available = vim.tbl_keys(parsers)
+                    if vim.tbl_contains(available, lang) then
+                        require("nvim-treesitter").install(lang)
                     end
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "TSUpdate",
+                callback = function()
+                    vim.bo.syntax = ""
+                    vim.treesitter.start()
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    -- vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    -- vim.wo[0][0].foldmethod = "expr"
                 end,
             })
 
