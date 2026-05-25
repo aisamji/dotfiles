cb7fed1d504c256baaa0388a7542cf0228869070
Ali Samji <aisamji@users.noreply.github.com>
2026-05-25 08:22:52 -0500

    fix(neovim): Remove conflicting keymaps

--- playbooks/files/nvim/lua/plugins/files.lua
+++ playbooks/files/nvim/lua/plugins/files.lua
@@ -1,9 +1,18 @@
 return {
     {
-        'stevearc/oil.nvim',
+        "stevearc/oil.nvim",
         ---@module 'oil'
         ---@type oil.SetupOpts
-        opts = {},
+        opts = {
+            skip_confirm_for_simple_edits = true,
+            keymaps = {
+                ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+                ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+                ["<C-s>"] = false,
+                ["<C-h>"] = false,
+                ["<C-l>"] = false,
+            },
+        },
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
         lazy = false,
