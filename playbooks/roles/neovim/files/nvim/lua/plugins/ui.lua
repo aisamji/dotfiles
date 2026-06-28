8cd9813eef43f935020156878fe76adc63bfecf3
Ali Samji <aisamji@users.noreply.github.com>
2026-06-27 21:57:44 -0500

    feat(neovim): Move lualine sections.
    - Make filename be relative
    - Enable oil extension (custom lualine for oil)

--- playbooks/roles/neovim/files/nvim/lua/plugins/ui.lua
+++ playbooks/roles/neovim/files/nvim/lua/plugins/ui.lua
@@ -19,35 +19,54 @@ return {
         "folke/which-key.nvim",
         event = "VeryLazy",
         config = function()
-            local wk = require("which-key")
-            wk.add({
+            local wk = require "which-key"
+            wk.add {
                 { "<leader>f", group = "fuzzy find" },
                 { "<leader>g", group = "git" },
                 { "<leader>d", group = "diagnostics" },
-            })
-        end
+            }
+        end,
     },
     {
-        'nvim-lualine/lualine.nvim',
+        "nvim-lualine/lualine.nvim",
         opts = {
             options = {
                 globalstatus = true,
-                theme = 'tokyonight',
+                theme = "tokyonight",
             },
             sections = {
-                lualine_x = { 'lsp_status', 'filetype' },
-                lualine_y = { 'encoding', 'fileformat' },
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff" },
+                lualine_c = {
+                    "diagnostics",
+                    {
+                        "filename",
+                        path = 1, -- Show as relative path
+                    },
+                },
+                lualine_x = { "filetype", "lsp_status" },
+                lualine_y = { "encoding", "fileformat" },
+                lualine_z = { "location" },
             },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            extensions = { "oil" },
         },
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
+        dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     {
-        'folke/tokyonight.nvim',
+        "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            require('tokyonight').setup {}
+            require("tokyonight").setup {}
             vim.cmd [[colorscheme tokyonight]]
-        end
+        end,
     },
 }
